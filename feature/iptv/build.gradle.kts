85a693936c7c81ff91ebf3b2eaed215282fca71e
khandev1211-cpu <khandev1211@gmail.com>
2026-07-07 00:14:12 +0500

    Step 8: Set up GitHub Actions CI, complete all module build files, and update gitignore

---
+++ feature/iptv/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt.android)
+    alias(libs.plugins.ksp)
+}
+
+android {
+    namespace = "com.streamcast.feature.iptv"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(project(":ui:theme"))
+    implementation(project(":core:player"))
+    implementation(project(":core:network"))
+
+    implementation(libs.androidx.core-ktx)
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+}
---
+++ feature/subtitles/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt.android)
+    alias(libs.plugins.ksp)
+}
+
+android {
+    namespace = "com.streamcast.feature.subtitles"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(project(":ui:theme"))
+    implementation(project(":core:player"))
+    implementation(project(":core:network"))
+    implementation(project(":core:database"))
+
+    implementation(libs.androidx.core-ktx)
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+}
